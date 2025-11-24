1577dc4e3994528d710bf313bcfeddb142ec5ecc
veritas <liorgby2@gmail.com>
2025-11-24 11:19:10 +0200

    first commit

---
+++ config/db/prod.js
@@ -0,0 +1,5 @@
+export default {
+    dbURL: process.env.MONGO_URL || '',
+    dbName: process.env.DB_NAME || 'test'
+}
+
---
+++ server.js
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+const corsOptions = {
+  origin: [
+    'http://127.0.0.1:5173',
+    'http://localhost:5173',
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.static('dist'));
+app.use(cookieParser());
+app.set('query parser', 'extended');
+
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.resolve('dist/index.html'));
+});
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server ready at http://localhost:${port}`));
