8f4b589bce941f96d8e3f6dfc566cec854b1c1d6
Chanan Welt <chwelt88@gmail.com>
2025-11-26 20:42:28 +0200

    update exported middleware function name

--- middleware/logger.middleware.js
+++ middleware/logger.middleware.js
@@ -1,6 +1,6 @@
-import { loggerService } from "../services/logger.service.js";
+import { loggerService } from '../services/logger.service.js';
 
-export function log(req, res, next) {
+export function logRequest(req, res, next) {
   loggerService.info(
     `Path triggered: ${req.method} ${req.originalUrl} from ${req.ip}`
   );
