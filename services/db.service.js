8f02f7a8afd5bd9ca0385e2ce5e9b4802d05fbf5
Chanan Welt <chwelt88@gmail.com>
2025-11-26 20:12:56 +0200

    log db errors via logger service

--- services/db.service.js
+++ services/db.service.js
@@ -1,7 +1,7 @@
-import { MongoClient } from "mongodb";
-import { loggerService } from "../services/logger.service.js";
+import { MongoClient } from 'mongodb';
+import { loggerService } from '../services/logger.service.js';
 
-import { config } from "../config/db/index.js";
+import { config } from '../config/db/index.js';
 // console.log('config:', config)
 
 export const dbService = { getCollection };
@@ -14,7 +14,7 @@ async function getCollection(collectionName) {
     const collection = await db.collection(collectionName);
     return collection;
   } catch (err) {
-    loggerService.error("Failed to get Mongo collection", err);
+    loggerService.error('Failed to get Mongo collection', err);
     throw err;
   }
 }
@@ -26,7 +26,7 @@ async function _connect() {
     const client = await MongoClient.connect(config.dbURL);
     return (dbConn = client.db(config.dbName));
   } catch (err) {
-    loggerService.error("Cannot Connect to DB", err);
+    loggerService.error('Cannot Connect to DB', err);
 
     throw err;
   }
