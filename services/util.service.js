6117e6d0ac4ed09b99d2199273100f97fb3e4d0e
Chanan Welt <chwelt88@gmail.com>
2025-12-10 13:26:15 +0200

    add util function for extracting timestamp fron an object ID, and converting it from UTC to israel time zone

--- services/util.service.js
+++ services/util.service.js
@@ -1,11 +1,21 @@
 export const utilService = {
   removeEmptyObjectFields,
+  getObjTimestamp,
 };
 
-function removeEmptyObjectFields(obj) {
+export function removeEmptyObjectFields(obj) {
   return Object.fromEntries(
     Object.entries(obj).filter(
       ([key, value]) => value !== null && value !== undefined
     )
   );
+}
+
+// Helper function to get the createdAt timestamp from an object's ID and converting it from UTC to Israel timezone
+export function getObjTimestamp(obj) {
+  const utcDate = obj._id.getTimestamp();
+  const israelDate = new Date(
+    utcDate.toLocaleString('en-US', { timeZone: 'Asia/Jerusalem' })
+  );
+  return israelDate;
 }
